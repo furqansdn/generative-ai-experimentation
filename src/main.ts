bf65fb60eb92aae578d4a19a09aad28437b6155b
Furqan Soedono <furqonsoedono71@gmail.com>
2024-05-09 13:04:38 +0700

    build: add helmet and typeorm-transactional

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import compression from 'compression';
+import helmet from 'helmet';
+import { initializeTransactionalContext } from 'typeorm-transactional';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  initializeTransactionalContext();
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+
+  app.use(helmet());
+
+  app.use(compression());
   await app.listen(3000);
 }
 bootstrap();
